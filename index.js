664239c805636b3fe3c884e8d4d7670e98bd3fb2
SwordGenius <swordgenius222@gmail.com>
2023-12-06 09:24:54 -0600

    arreglo 1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-app.use('/alumnos', require('./routes/alumno.route'));
+app.use('/alumnos', require('./src/routes/alumno.route'));
 
 app.listen(process.env.PORT||3000,() => {
     console.log("Servidor corriendo en el puerto 3000");
